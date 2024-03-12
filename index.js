6ba149f023f458980a1d6634e002b6566e4916ea
Shashidhar Angadi <shashidharangadi14@gmail.com>
2024-03-12 23:37:38 +0530

    Initilal files

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+
+client.on('messageCreate',(message)=>{
+    if(message.author.bot) return ;
+    message.reply({
+        content: "Hi from Bot"
+    })
+});
+
+client.on('interactionCreate',interaction=>{
+    console.log(interaction);
+    interaction.reply('pong');
+})
+
+client.login( process.env.token );
